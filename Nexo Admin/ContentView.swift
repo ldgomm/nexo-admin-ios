bdd6310fa76cf2ed26a1d7df3279827f8a246951
José Ruiz <ruiz211@protonmail.com>
2026-05-20 19:53:02 -0500

    Initial Commit

---
+++ Nexo Admin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nexo Admin
+//
+//  Created by José Ruiz on 20/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nexo Admin/Nexo_AdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nexo_AdminApp.swift
+//  Nexo Admin
+//
+//  Created by José Ruiz on 20/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Nexo_AdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
